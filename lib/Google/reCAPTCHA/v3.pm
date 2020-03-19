497faf755b30a2ca91fc5e5b05eec91bee07ce59
justingit <perl@justinsimoni.com>
2020-03-19 14:09:05 -0600

    Minilified!

--- lib/Google/reCAPTCHA/v3.pm
+++ lib/Google/reCAPTCHA/v3.pm
@@ -1,5 +1,6 @@
 package Google::reCAPTCHA::v3;
 
+my $VERSION = '0.1.0';
 use warnings; 
 use strict;
 
@@ -94,5 +95,121 @@ sub request {
 	);	
 }
 
-	
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::reCAPTCHA::v3 - A simple Perl API for Google reCAPTCHA v3
+
+=head1 SYNOPSIS
+
+    use Google::reCAPTCHA::v3;
+
+	my $grc = Google::reCAPTCHA::v3->new(
+        {
+            -secret => 'Google reCAPTCHA v3 site secret key',
+        }
+    );
+
+	my $r = $grc->request(
+		{ 
+			-response => 'response_token',
+			-remoteip => $remote_ip, # optional 
+		}
+	); 
+
+=head1 DESCRIPTION
+
+Google reCAPTCHA v3 is a simple module that is used to verify the reCAPTCHA response token generated from the front end 
+of your app.
+
+See: L<https://developers.google.com/recaptcha/docs/verify>. 
+
+=head1 METHODS
+
+=head2 new
+
+	my $grc = Google::reCAPTCHA::v3->new(
+        {
+            -secret => 'Google reCAPTCHA v3 site secret key',
+        }
+    );
+
+Requires one paramater, C<-secret>, which should be your Google reCAPTCHA v3 site secret key. 
+Returns a new Google::reAPTCHA::v3 object. 
+	
+=head2 request
+
+	my $r = $grc->request(
+		{ 
+			-response => 'response_token',
+			-remoteip => $remote_ip, # optional 
+		}
+	); 
+
+	if($r->{success} == 1){ 
+		# do useful things, like check the score
+	}
+	else { 
+		# well, that didn't work. 
+	}
+
+Requires one paramater, C<-response>, which should be the reCAPTCHA response token generated from the front end 
+of your app. 
+
+Optionally, you can pass, C<-remoteip>, which should be your user's IP address.
+
+request returns a hashref of what's return from the service, with the following keys: 
+
+=over
+
+=item * success
+
+C<1> (valid) or C<0> (invalid). 
+
+Whether this request was a valid reCAPTCHA token for your site
+
+=item * score
+
+C<number> 
+
+The score for this request (0.0 - 1.0)
+
+=item * action 
+
+The action name for this request (important to verify)
+
+=item * challenge_ts
+
+Timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+
+=item * hostname
+
+The hostname of the site where the reCAPTCHA was solved
+
+=item * error-codes
+
+=back
+
+=head1 LICENSE
+
+Copyright (C) Justin Simoni
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 BUGS
+
+Please file any bugs/issues within the github repo: L<https://github.com/justingit/Google-reCAPTCHA-v3/issues>
+
+=head1 AUTHOR
+
+Justin Simoni
+
+=cut
